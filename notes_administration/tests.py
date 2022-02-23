2c37b695f2604c39ed44b54d48166466b2dc807c
Lena Martinson <lmartinson@yandex.ru>
2022-02-23 17:05:26 +0300

    tests work

--- notes_administration/tests.py
+++ notes_administration/tests.py
@@ -1,19 +1,21 @@
 from django.test import TestCase
 from .models import Note, Users
+from django.test import Client
 
 class CreatingDb(TestCase):
-    #def setUp(self):
+    def setUp(self):
         Note.objects.all().delete()
         Users.objects.all().delete()
-        Users.objects.create(name="Ann", email="ann@ann.ann", users_id=1)
-        Users.objects.create(name="Jane", email="jane@jane.jane", users_id=2)
-        Users.objects.create(name="Nick", email="nick@nick.nick", users_id=3)
-        Note.objects.create(author=Users.objects.get(users_id=1), text="good", to_whom=3)
+        Users.objects.create(name="Ann", email="ann@ann.ann", user_id=1)
+        Users.objects.create(name="Jane", email="jane@jane.jane", user_id=2)
+        Users.objects.create(name="Nick", email="nick@nick.nick", user_id=3)
+        Note.objects.create(author=Users.objects.get(user_id=1), text="good", to_whom=3, note_id=1)
 
     #def tearDown(self):
     #    pass
    
 
-    #def test_something(self):
-    #    self.assertEqual(1, 1)
+    def test_something(self):
+        c = Client()
+        c.get('/api/notes/?format=json')
    
