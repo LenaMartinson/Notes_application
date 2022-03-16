5a0fc3482fd5f428a7210c55f7f7e516953e28c8
Lena Martinson <lmartinson@yandex.ru>
2022-03-16 19:09:35 +0300

    update script make_table.sh

--- make_table.sh
+++ make_table.sh
@@ -2,15 +2,17 @@
 
 export PGPASSWORD=123
 
-echo "delete from notes_administration_users;" | psql -h localhost -U kena -p 5433 -d notes_database_2
-echo "insert into notes_administration_users values 
-('lena', 'lena@lena.lena', 1);" | psql -h localhost -U kena -p 5433 -d notes_database_2
-echo "insert into notes_administration_users values 
-('ira', 'ira@lena.lena', 2);" | psql -h localhost -U kena -p 5433 -d notes_database_2
-echo "select * from notes_administration_users;" | psql -h localhost -U kena -p 5433 -d notes_database_2
+psql --file=commands.txt -h localhost -U kena -p 5433 -d notes_database_2
 
-echo "delete from notes_administration_note;" | psql -h localhost -U kena -p 5433 -d notes_database_2
-echo "insert into notes_administration_note values 
-('good', 'iraa', 1, 2);" | psql -h localhost -U kena -p 5433 -d notes_database_2
-echo "select * from notes_administration_note;" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "delete from notes_administration_users;" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "insert into notes_administration_users values 
+#('lena', 'lena@lena.lena', 1);" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "insert into notes_administration_users values 
+#('ira', 'ira@lena.lena', 2);" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "select * from notes_administration_users;" | psql -h localhost -U kena -p 5433 -d notes_database_2
+
+#echo "delete from notes_administration_note;" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "insert into notes_administration_note values 
+#('good', 'iraa', 1, 2);" | psql -h localhost -U kena -p 5433 -d notes_database_2
+#echo "select * from notes_administration_note;" | psql -h localhost -U kena -p 5433 -d notes_database_2
 
