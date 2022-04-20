448e45f578cfee5fdca583a387fc199f810a09c6
Lena Martinson <lmartinson@yandex.ru>
2022-04-20 19:55:59 +0300

    updated extension files

--- extension_files/background.js
+++ extension_files/background.js
@@ -5,23 +5,32 @@ function reddenPage() {
   let centerY = document.documentElement.clientHeight / 2;
 
   let elem = document.elementFromPoint(centerX, centerY);
-
   elem.style.background = "red";
   */
+  //document.body.style.backgroundColor = 'lime';
+  let text = "Hi Lena!";
+  let url = "http://127.0.0.1:8000/api/notes/?format=json";
+  let response = fetch(url);
+  if (response.ok) {
+    text = response.text();
+  } else {
+    text = "bad request";
+  }
   let message = document.createElement('input');
   message.setAttribute("type", "text");
-  message.setAttribute("value", "Hi Lena!");
+  message.setAttribute("value", text);
   var sp2 = document.getElementById("profile-content");
   var main2 = document.getElementsByClassName("artdeco-card ember-view pv-top-card");
   var parent = main2[0].parentNode;
   parent.insertBefore(message, main2[0].nextSibling);
   let button = document.createElement('button');
   button.innerHTML = "Submit";
-  button.style = 'yellow';
+  //button. = 'yellow';
   button.onclick = function () {
     alert("Button is clicked");
   }
-  parent.insertBefore(button, message);    
+  parent.insertBefore(button, message);
+  
 }
    
 chrome.action.onClicked.addListener((tab) => {
