a64ff45d2af33eccfa523e8855e7b4c586ad61d9
Lena Martinson <lmartinson@yandex.ru>
2022-04-27 21:16:13 +0300

    find ids and push right inform in note window

--- extension_files/background.js
+++ extension_files/background.js
@@ -9,18 +9,30 @@ async function reddenPage() {
   */
   //document.body.style.backgroundColor = 'lime';
   let doc_url_path = document.location.pathname; // чтобы взять потом имя и искать по нему
+  user_name = doc_url_path.split('/')[2];
+  var source = document.body.innerHTML;
+  console.log(user_name);
+  var my_name = source.match(/(publicIdentifier":")\w*(")/)[0];
+  my_name = my_name.split('"')[2];
+  console.log(my_name); 
+
 
   let text = "No information";
-  let url = "http://127.0.0.1:8000/api/notes/?format=json";
-  let response = await fetch(url);
-  console.log(response);
-  if (response.ok) {
-    let all_json = await response.json();
-    //all_json = json.parse(all_json);
-    //console.log(all_json);
-    let data = all_json.find(item => item.to_whom === "lena")
-    //console.log(data);
-    text = data["text"];
+  let url_notes = "http://127.0.0.1:8000/api/notes/?format=json";
+  let url_users = "http://127.0.0.1:8000/api/users/?format=json";
+  let response = await fetch(url_notes);
+  let response_users = await fetch(url_users);
+  if (response.ok && response_users.ok) {
+    let all_notes_json = await response.json();
+    let all_users_json = await response_users.json();
+    console.log("all is ok");
+    let user_id = all_users_json.find(item => item.name == my_name); //пока живем с идеей что user точно есть в таблице
+    console.log(user_id["user_id"]);
+    let data = all_notes_json.filter(item => item.author === user_id["user_id"]);
+    let data2  = data.filter(item => item.to_whom === user_name); // пришлось разделить на 2, может это можно исправить
+    console.log(data2[0]);
+    //console.log(data2);
+    text = data2[0]["text"];
   } else {
     text = "bad request";
   }
